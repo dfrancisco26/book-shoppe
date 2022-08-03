8a190c0adb1705d05e1582f8dd334b5bfcbf5914
David Francisco <dfrancisco2677@gmail.com>
2022-08-03 12:13:43 -0700

    Created SQL database. Struggled with some password error, but it was just me needing to delete the example from the .env

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,21 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
+const { request } = require('express');
 // const request = require('supertest');
-// const app = require('../lib/app');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('book routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('/books should give a list of books', async () => {
+    const resp = await request(app).get('/books'); 
+    expect(resp.body.length).toEqual(7);
+  
   });
+
+  
   afterAll(() => {
     pool.end();
   });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,19 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP table if exists books;
+
+CREATE table books (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR,
+    author VARCHAR,
+    released INT
+);
+
+INSERT INTO books (title, author, released) VALUES
+('SQL for dummies', 'D.F.', 1992),
+('Supabase goes hard', 'Alt Tab Group', 2022),
+('Can I get a witness?', 'D.F.', 1995),
+('Y2K wuz sick', 'D.F.', 2001),
+('Heroku cool too', 'Alt Tab Group', 2021),
+('Wait what', 'D.F.', 2019),
+('my only book', 'Iglus Ran', 2024);
