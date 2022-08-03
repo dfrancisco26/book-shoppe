ebdd94147ce81273681983b11f44300025d7cfd3
David Francisco <dfrancisco2677@gmail.com>
2022-08-03 14:45:33 -0700

    GetAll books route works. Tests for length of books and passes.

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -1,21 +1,21 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-const { request } = require('express');
-// const request = require('supertest');
+const request = require('supertest');
 const app = require('../lib/app');
 
 describe('book routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-
-  it('/books should give a list of books', async () => {
-    const resp = await request(app).get('/books'); 
+  it('/books should return a list of books', async () => {
+    const resp = await request(app).get('/books');
     expect(resp.body.length).toEqual(7);
-  
+    expect(resp.body[0]).toEqual({
+      id: expect.any(String),
+      title: expect.any(String)
+    });
   });
 
-  
   afterAll(() => {
     pool.end();
   });
