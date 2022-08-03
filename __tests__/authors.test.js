3d2aa247eda361b9014c5f4ffd3c01bf66e5abc2
David Francisco <dfrancisco2677@gmail.com>
2022-08-03 15:23:15 -0700

    Controllers and models for Author getAll properly. Test passes.

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -7,6 +7,20 @@ describe('author routes', () => {
   
   beforeEach(() => {
     return setup(pool);
-    
   });
+
+  it('returns a list of authors', async () => {
+    const res = await request(app).get('/authors');
+    expect(res.body.length).toEqual(3);
+    expect(res.body[0]).toEqual({
+      id: expect.any(String),
+      name: expect.any(String)
+    });
+  });
+
+  
+  afterAll(() => {
+    pool.end();
+  });
+
 });
